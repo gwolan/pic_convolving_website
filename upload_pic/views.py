49a2f66117ac41a2a3fc86232583e162f7e46ffc
Grzegorz Wolanski <grzegorz.wolanski4321@wp.pl>
2020-12-26 01:20:52 +0100

    Added form with POST method to frontend HTML

--- upload_pic/views.py
+++ upload_pic/views.py
@@ -3,9 +3,13 @@ from .src.EffectType import EffectType
 
 
 def upload_pic(request):
+    supported_effects = list(EffectType.__members__.keys())
+
     context = {
         'title': 'Witaj!',
         'effects': EffectType.__members__,
+        'checked': ' checked',
+        'first_checkbox_value': EffectType.__members__[supported_effects[0]].value[1],
     }
 
     return render(request, 'upload_pic/homepage.html', context)
