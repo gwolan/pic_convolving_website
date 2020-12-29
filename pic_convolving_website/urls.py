dab66262969b9725447fb36d41971ec4afa58b5e
Grzegorz Wolanski <grzegorz.wolanski4321@wp.pl>
2020-12-29 21:48:17 +0100

    Added support for favicon.ico

--- pic_convolving_website/urls.py
+++ pic_convolving_website/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path, include
+from django.views.generic import RedirectView
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include('upload_pic.urls')),
+    re_path(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
 
 
