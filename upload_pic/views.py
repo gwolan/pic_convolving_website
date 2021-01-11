a2d2b0db9138bbf0c975b19fe6521a6e6f54fdf5
Grzegorz Wolanski <grzegorz.wolanski4321@wp.pl>
2021-01-11 02:02:22 +0100

    Added reading executable path from config.json

--- upload_pic/views.py
+++ upload_pic/views.py
@@ -1,3 +1,4 @@
+import subprocess, os
 from django.shortcuts import render
 from django.contrib import messages
 from .src.ChooseEffectRadioForm import radio_list
@@ -9,7 +10,6 @@ from .src.Config import engine_error_codes
 from .src.FileRemover import FileRemover
 from .src.ImageData import ImageData
 from .src.ImageValidator import ImageValidator
-import subprocess
 import pic_convolving_website.settings as settings
 
 
@@ -33,9 +33,9 @@ def get_result_context(image_url, effect_type):
 def modify_image(request, image_validator, image_data, chosen_option):
     if image_validator.is_image_valid():
         process_result = subprocess.run(
-            [settings.MEDIA_ROOT + str("/bin"), "-p", image_data.image_path,
-                                                "-f", image_validator.image.format.lower(),
-                                                "-e", chosen_option])
+            [os.path.join(settings.BASE_DIR, settings.config["EXECUTABLE_PATH"]), "-p", image_data.image_path,
+                                                                                  "-f", image_validator.image.format.lower(),
+                                                                                  "-e", chosen_option])
 
         if process_result.returncode == 0:
             return get_result_context(image_data.image_url, EffectType.__members__[chosen_option].value[0])
